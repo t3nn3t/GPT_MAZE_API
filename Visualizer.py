dbf95eb09655127712af10f77987fb9b75757b6e
Jake <41425888+t3nn3t@users.noreply.github.com>
2024-02-01 14:54:29 +0000

    using matplotlib instead

--- Visualizer.py
+++ Visualizer.py
@@ -1,39 +1,24 @@
-import pygame_matplotlib
-import pygame
-import matplotlib
-matplotlib.use('module://pygame_matplotlib.backend_pygame')
+import pandas as pd
 import matplotlib.pyplot as plt
-
+from itertools import count
 
 class Visualizer:
     def __init__(self):
-        width, height = 900, 600
-        self.fps = 60
-    
-        self.window = pygame.display.set_mode((width, height))
-        pygame.display.set_caption("Maze Visualizer")
+        
+        plt.style.use('fivethirtyeight')
 
     def start_simul(self):
-        clock = pygame.time.Clock()
-        run = True
-        while run:
-            clock.tick(self.fps)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-        
-            self.draw_window()
-
-        pygame.quit()
+        self.draw_window()
 
 
     def draw_window(self):
-        self.window.fill((255,200,255))
-        fig, axes = plt.subplots(1, 1,)
-        axes.plot([1,2], [1,2], color='green', label='test')
-        
-        self.window.blit(fig, (100, 100))
-        pygame.display.update()
+        x_vals = [0,1,2,3,4,5]
+        y_vals = [0,1,2,3,4,5]
+        print("plotting...")
+        plt.plot(x_vals, y_vals)
+
+        plt.tight_layout()
+        plt.show()
 
 
 
